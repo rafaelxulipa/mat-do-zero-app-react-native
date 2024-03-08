dfa77863677de49c5c9d25633395de7fc052b47d
Otavio Melo <otavio.melo@meisters.solutions>
2024-03-08 10:41:47 -0300

    updated screen size

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
+import { View, StyleSheet, StatusBar } from 'react-native';
 
 export default function App() {
   const url = 'https://cursomatematicadozero.or.app.br/';
 
   return (
-    <WebView
-      source={{ uri: url }}
-      style={{ flex: 1 }}
-      javaScriptEnabled={true}
-      domStorageEnabled={true}
-    />
+    <View style={styles.container}>
+      <StatusBar hidden />
+
+      <WebView
+        source={{ uri: url }}
+        style={{ flex: 1 }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
